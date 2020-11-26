0ce5fc9288168d044a7c1ddcddb3fb9ef9e27f97
Goderich <yuhsien77@gmail.com>
2020-11-26 11:38:29 +0800

    Add day 4

---
+++ day4.rkt
@@ -0,0 +1,20 @@
+#lang racket/base
+
+(require openssl/md5)
+
+(define starts-with-zeros
+  (lambda (i #:numzeros (zs 5))
+    (equal? (make-string zs #\0)
+            (substring (md5 (open-input-string (format "bgvyzdsv~a" i))) 0 zs))))
+
+;; part 1
+
+(for/first ((i (in-naturals))
+            #:when (starts-with-zeros i))
+  i)
+
+;; part 2
+
+(for/first ((i (in-naturals))
+            #:when (starts-with-zeros i #:numzeros 6))
+  i)
