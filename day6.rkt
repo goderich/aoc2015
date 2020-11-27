3526ad7b4afa4ed5065921f1a547b4d505b04742
Goderich <yuhsien77@gmail.com>
2020-11-27 13:28:53 +0800

    Finish day 6

--- day6.rkt
+++ day6.rkt
@@ -3,19 +3,36 @@
 (require megaparsack
          megaparsack/text
          data/monad
-         data/applicative)
+         data/applicative
+         racket/match
+         racket/file
+         racket/list)
 
 (struct coord (x y) #:transparent)
+(struct instruction (flag nw-point se-point) #:transparent)
 
 (define (matrix nw se)
   (for*/list ((x (in-range (coord-x nw) (add1 (coord-x se))))
               (y (in-range (coord-y nw) (add1 (coord-y se)))))
     (coord x y)))
 
-(define grid
-  (make-hash
-   (map (λ (x) (cons x 0))
-        (matrix (coord 0 0) (coord 999 999)))))
+(define (execute! f point)
+  (match f
+    ('on (hash-set! grid point 1))
+    ('off (hash-set! grid point 0))
+    ('toggle (hash-update! grid point
+                           (λ (x) (if (zero? x) 1 0))))))
+
+(define (run-instruction str handler)
+  (define result
+    (parse-result!
+     (parse-string instruction/p str)))
+  (define flag (instruction-flag result))
+  (define points (matrix
+                  (instruction-nw-point result)
+                  (instruction-se-point result)))
+  (for ((point points))
+    (handler flag point)))
 
 (define on/p
   (do (string/p "on ")
@@ -34,11 +51,47 @@
   (do (string/p "toggle ")
       (pure 'toggle)))
 
-(define matrix/p
-  (pure 'TODO))
+(define coord/p
+  (do (x <- integer/p)
+      (char/p #\,)
+      (y <- integer/p)
+      (pure (coord x y))))
 
 (define instruction/p
   (do (flag <- (or/p (try/p toggle/p)
                      switch/p))
-      (block <- (matrix/p))
-    (pure)))
+      (nw-point <- coord/p)
+      (string/p " through ")
+      (se-point <- coord/p)
+      (pure (instruction flag nw-point se-point))))
+
+;; part 1
+
+(define grid
+  (make-hash
+   (map (λ (x) (cons x 0))
+        (matrix (coord 0 0) (coord 999 999)))))
+
+(for ((line (file->lines "inputs/day6.txt")))
+  (run-instruction line execute!))
+
+(count positive? (hash-values grid))
+
+;; part 2
+
+(define (execute2! f point)
+  (match f
+    ('toggle (hash-update! grid point (λ (x) (+ 2 x))))
+    ('on (hash-update! grid point add1))
+    ('off (hash-update! grid point
+                        (λ (x) (if (zero? x)
+                                   0
+                                   (sub1 x)))))))
+
+;; Reset the grid for part 2
+(run-instruction "turn off 0,0 through 999,999" execute!)
+
+(for ((line (file->lines "inputs/day6.txt")))
+  (run-instruction line execute2!))
+
+(apply + (hash-values grid))
