74cbd25bdd17664404ae40295ef7148ab074aff1
Goderich <yuhsien77@gmail.com>
2020-12-01 21:06:30 +0800

    Finish day 9

--- day9.rkt
+++ day9.rkt
@@ -5,15 +5,36 @@
          racket/list
          threading)
 
-(define origins-distances
+(define edges
  (for/list ((line (file->lines "inputs/day9.txt")))
    (define words (string-split line))
    (define origin (first words))
+   (define destination (third words))
    (define distance (string->number (last words)))
-   (cons origin distance)))
+   (list origin destination distance)))
 
-(define origins
-  (remove-duplicates (map car origins-distances)))
+(define vertices
+  (~>> edges
+       (map car)
+       (remove-duplicates)
+       (cons (second (last edges)))))
 
-(for/list ((city origins))
-  (filter (λ (x) (equal? city (car x))) origins-distances))
+(define (find-edge v1 v2)
+  (for/first ((edge edges)
+              #:when (and (member v1 edge)
+                          (member v2 edge)))
+    (last edge)))
+
+(define distances
+  (for/list ((vs (permutations vertices)))
+    (for/sum ((v1 (drop-right vs 1))
+              (v2 (rest vs)))
+      (find-edge v1 v2))))
+
+;; part 1
+
+(apply min distances)
+
+;; part 2
+
+(apply max distances)
